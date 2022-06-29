a0fdc21a958f6184e9b40b680fef90f4c8047c99
Vamshikrishna-surepalli <107537681+Vamshikrishna-surepalli@users.noreply.github.com>
2022-06-29 19:48:31 +0530

    Add files via upload

---
+++ primenumber.js
@@ -0,0 +1,18 @@
+const number = parseInt('13');
+let isPrime = true;
+if (number === 1) {
+    console.log("1 is neither prime nor composite number.");
+}
+else if (number > 1) {
+    for (var i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(number+ ' '+'is a prime number');
+    } else {
+        console.log(number+ ' '+'is not a prime number');
+    }
+}
